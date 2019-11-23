3033ac30f964c9117fc0bf6a19ffa793cc5fd6b5
Ashley Donald <ashd32@gmail.com>
2019-11-23 13:50:42 -0500

    Updates

---
+++ precache-manifest.194a8173ed292edccd16d57fb3dd69ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "081034a38fd9a32f43ba",
+    "url": "/goodhelpv2/static/css/main.e35ec0cc.chunk.css"
+  },
+  {
+    "revision": "081034a38fd9a32f43ba",
+    "url": "/goodhelpv2/static/js/main.081034a3.chunk.js"
+  },
+  {
+    "revision": "ac14e3cc6d5120ff1677",
+    "url": "/goodhelpv2/static/js/1.ac14e3cc.chunk.js"
+  },
+  {
+    "revision": "3f91a86eeb69fc5ef726",
+    "url": "/goodhelpv2/static/js/runtime~main.3f91a86e.js"
+  },
+  {
+    "revision": "f9161e8018394c7147e540c1a2dcb83e",
+    "url": "/goodhelpv2/index.html"
+  }
+];
